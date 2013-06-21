b2877af942869cf070c1520c5e622d7ea33d8350
curtismchale <curtis@curtismchale.ca>
2013-06-21 15:53:17 -0700

    - 1st post type for users
    Now we have a post type to start storing user data in

--- class/class.WP_Proj_Users.php
+++ class/class.WP_Proj_Users.php
@@ -4,6 +4,53 @@ class WP_Proj_Users{
 
 	function __construct(){
 
+		add_action( 'init', array( $this, 'user_cpt' ) );
+
 	} // __construct
 
+	/**
+	 * Builds out the custom post types for the site
+	 *
+	 * @uses    register_post_type
+	 *
+	 * @since   1.0
+	 * @author  SFNdesign, Curtis McHale
+	 */
+	public function user_cpt(){
+
+		$labels = array(
+			'name'                  => __('WP Proj Users'),
+			'singular_name'         => __('WP Proj User'),
+			'add_new'               => __('Add New'),
+			'add_new_item'          => __('Add New WP Proj User'),
+			'edit'                  => __('Edit'),
+			'edit_item'             => __('Edit WP Proj User'),
+			'new_item'              => __('New WP Proj User'),
+			'view'                  => __('View WP Proj User'),
+			'view_item'             => __('View WP Proj User'),
+			'search_items'          => __('Search WP Proj Users'),
+			'not_found'             => __('No WP Proj Users Found'),
+			'not_found_in_trash'    => __('No WP Proj Users found in Trash')
+		);
+
+		$arguments = array(
+			'labels'                => apply_filters( 'wpproj_register_user_cpt_labels', $labels ),
+			'public'                => true,
+			'menu_position'         => 5, // sets admin menu position
+			//'menu_icon'           => get_stylesheet_directory_uri().'/assets/images/show-post-icon.png',
+			'hierarchical'          => false, // funcions like posts
+			'supports'              => array('title', 'editor', 'revisions', 'excerpt', 'thumbnail'),
+			'rewrite'               => array('slug' => 'clients', 'with_front' => true,), // permalinks format
+			'can_export'            => true,
+		);
+
+		register_post_type(
+			'wpproj_users',
+			apply_filters( 'wpproj_register_user_cpt_args', $arguments )
+		);
+
+	} // new_cpt
+
 } // WP_Proj_Users
+
+new WP_Proj_Users();
